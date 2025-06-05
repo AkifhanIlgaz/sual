a76a4a3635caeb35b5b459322f17795229415ef6
AkifhanIlgaz <akifhanilgaz@gmail.com>
2025-06-05 09:29:13 +0300

    Add Vercel Analytics integration to layout and update package.json dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Metadata, Viewport } from 'next'
 import { fontSans } from '@/config/fonts'
 import { siteConfig } from '@/config/site'
 // import { Image } from '@heroui/image'
+import { Analytics } from '@vercel/analytics/next'
 import { Providers } from './providers'
 
 export const metadata: Metadata = {
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<head />
 			<body className={clsx('min-h-screen text-foreground bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-sky-900 via-zinc-800 to-pink-800 font-sans antialiased', fontSans.variable)}>
 				<Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+					<Analytics />
 					<div className="relative flex flex-col items-center justify-center min-h-screen">
 						{/* <Image src={`logo.png`} alt={'logo'} width={`250`} height={`250`} className="brightness-0 invert" /> */}
 						<main className="container mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 pt-8 sm:pt-16 flex-grow">{children}</main>
