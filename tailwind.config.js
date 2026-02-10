7726bfcb5c3438c1967d16c454e8bfc80e6a38c5
M. Akifhan Ilgaz <akifhanilgaz@gmail.com>
2026-02-10 16:26:54 +0300

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const { heroui } = require('@heroui/theme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			fontFamily: {
@@ -11,6 +10,5 @@ module.exports = {
 			}
 		}
 	},
-	darkMode: 'class',
-	plugins: [heroui()]
+	darkMode: 'class'
 }
